f91fb6ea1509692d23225188cabde9bc8fe49fa0
10mohi6 <10.mohi.6.y@gmail.com>
2021-09-13 09:05:49 +0900

    first commit

---
+++ tests/char.rs
@@ -0,0 +1,34 @@
+use std::io::Cursor;
+use wordcount::{count, CountOption};
+
+#[macro_use]
+mod utils;
+
+#[test]
+fn char_count_works() {
+    let input = Cursor::new(b"abadracadabra");
+    let freq = count(input, CountOption::Char);
+    assert_map!(freq,{
+        "a" => 6,
+        "b" => 2,
+        "c" => 1,
+        "d" => 2,
+        "r" => 2
+    });
+}
+
+#[test]
+fn char_count_utf8() {
+    let input = Cursor::new(
+        r#"
+天地
+宇宙
+月日
+"#,
+    );
+    let freq = count(input, CountOption::Char);
+    assert_eq!(freq.len(), 6);
+    for (_, count) in freq {
+        assert_eq!(count, 1);
+    }
+}
